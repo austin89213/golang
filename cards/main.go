04d566713af87bc8d6f4f0b66c44610d802855dc
austinlin <austin89213@gmail.com>
2021-09-08 15:42:57 +0000

    New: Arrays / Slices and Forloop

--- cards/main.go
+++ cards/main.go
@@ -2,12 +2,21 @@ package main
 
 import "fmt"
 
-func mian() {
-	card := newCard()
+func main() {
+	// card := newCard()
+	// fmt.Println(card)
+	cards := []string{"Ace of Diamonds", newCard()}
+	// fmt.Println(cards)
+	cards = append(cards, "Six of Spades")
+	// fmt.Println(cards)
 
-	fmt.Println(card)
+	// Iterate a slice or list
+
+	for index, card := range cards {
+		fmt.Println(index, card)
+	}
 }
 
-func newCard() {
+func newCard() string {
 	return "Five of Diamonds"
 }
