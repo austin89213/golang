20dce7f9af3d18f118984dcb61cae46096ad5568
Austin <austin89213@gmail.com>
2021-10-01 09:42:42 +0800

    feat: New Type Deck and related func newDeck
    what changed
    1.main.go:Separate code related to deck to deck.go.
    2.deck.go:New type deck and func newDeck to create a complete deck.

---
+++ cards/deck.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+// Create a new type of 'deck' which is a slice of strings
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"♠️", "♥️", "♦️", "♣️"}
+	cardValues := []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, suit+value)
+		}
+	}
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
--- cards/main.go
+++ cards/main.go
@@ -1,22 +1,6 @@
 package main
 
-import "fmt"
-
 func main() {
-	// card := newCard()
-	// fmt.Println(card)
-	cards := []string{"Ace of Diamonds", newCard()}
-	// fmt.Println(cards)
-	cards = append(cards, "Six of Spades")
-	// fmt.Println(cards)
-
-	// Iterate a slice or list
-
-	for index, card := range cards {
-		fmt.Println(index, card)
-	}
-}
-
-func newCard() string {
-	return "Five of Diamonds"
+	cards := newDeck()
+	cards.print()
 }
